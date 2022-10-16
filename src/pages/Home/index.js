465aa1af2241d670faa0a306fccb7aaa2ca40b57
Hamidah <hamidah@socar.my>
2022-10-16 20:07:17 +0800

    create dummy page for fun

---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './home.css'
+
+/**
+ * Just dummy page
+ * @returns 
+ */
+export default function HomePage() {
+  return (
+    <div className="main">
+       <h1 className="notes__title">Home</h1>
+    </div>
+  )
+}
